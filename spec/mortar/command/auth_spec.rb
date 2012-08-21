6939310e61c15e0fb61a6d7abe844fc14c361502
Doug Daniels <ddaniels@mortardata.com>
2012-08-21 19:17:23 -0400

    Allow users to see their api key via mortar auth:key

--- spec/mortar/command/auth_spec.rb
+++ spec/mortar/command/auth_spec.rb
@@ -3,6 +3,17 @@ require "mortar/command/auth"
 
 describe Mortar::Command::Auth do
   
+  describe "auth:key" do
+    it "displays the user's api key" do
+      mock(Mortar::Auth).password {"foo_api_key"}
+      stderr, stdout = execute("auth:key")
+      stderr.should == ""
+      stdout.should == <<-STDOUT
+foo_api_key
+STDOUT
+    end
+  end
+    
   describe "auth:whoami" do
     it "displays the user's email address" do
       mock(Mortar::Auth).user {"sam@mortardata.com"}
