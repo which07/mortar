0a4d9ec3b471d9e028fd4f28dec397bf68843906
Jeremy Karn <jkarn@mortardata.com>
2012-08-27 20:43:28 -0400

    describe command for cli

---
+++ lib/mortar/command/describe.rb
@@ -0,0 +1,80 @@
+require "mortar/command/base"
+require "mortar/snapshot"
+
+# manage pig scripts
+#
+class Mortar::Command::Describe < Mortar::Command::Base
+  
+  include Mortar::Snapshot
+    
+  # describe [PIGSCRIPT] [ALIAS]
+  #
+  # Describe the schema of an alias and all of its ancestors.
+  #
+  # -p, --parameter NAME=VALUE  # Set a pig parameter value in your script.
+  # -f, --param-file PARAMFILE  # Load pig parameter values from a file.
+  #
+  # Examples:
+  #
+  # $ mortar describe
+  # 
+  # TBD
+  #
+  def index
+    pigscript_name = shift_argument
+    alias_name = shift_argument
+    unless pigscript_name && alias_name
+      error("Usage: mortar describe PIGSCRIPT ALIAS\nMust specify PIGSCRIPT and ALIAS.")
+    end
+    validate_arguments!
+    validate_git_based_project!
+    pigscript = validate_pigscript!(pigscript_name)
+    git_ref = create_and_push_snapshot_branch(git, project)
+    
+    describe_id = nil
+    action("Starting describe", {:success => "started"}) do
+      describe_id = api.post_describe(project.name, pigscript.name, alias_name, git_ref, :parameters => pig_parameters).body["describe_id"]
+    end
+        
+    describe_result = nil
+    display
+    ticking(polling_interval) do |ticks|
+      describe_result = api.get_describe(describe_id, :exclude_result => true).body
+      is_finished =
+        Mortar::API::Describe::STATUSES_COMPLETE.include?(describe_result["status"])
+        
+      redisplay("Describe status: %s %s" % [
+        describe_result['status'],
+        is_finished ? " " : spinner(ticks)],
+        is_finished) # only display newline on last message
+      if is_finished
+        display
+        break
+      end
+    end
+    
+    case describe_result['status']
+    when Mortar::API::Describe::STATUS_FAILURE
+      error_message = "Describe failed with #{describe_result['error_type'] || 'error'}"
+      if line_number = describe_result["line_number"]
+        error_message += " at Line #{line_number}"
+        if column_number = describe_result["column_number"]
+          error_message += ", Column #{column_number}"
+        end
+      end
+      error_message += ":\n\n#{describe_result['error_message']}"
+      error(error_message)
+    when Mortar::API::Describe::STATUS_KILLED
+      error("Describe killed by user.")
+    when Mortar::API::Describe::STATUS_SUCCESS
+      web_result_url = describe_result['web_result_url']
+      display("Results available at #{web_result_url}")
+      action("Opening web browser to show results") do
+        require "launchy"
+        Launchy.open(web_result_url).join
+      end
+    else
+      raise RuntimeError, "Unknown describe status: #{describe_result['status']} for describe_id: #{describe_id}"
+    end
+  end
+end
---
+++ spec/mortar/command/describe_spec.rb
@@ -0,0 +1,119 @@
+require 'spec_helper'
+require 'fakefs/spec_helpers'
+require 'mortar/command/describe'
+require 'mortar/api/describe'
+require 'launchy'
+
+module Mortar::Command
+  describe describe do
+    
+    before(:each) do
+      stub_core      
+      @git = Mortar::Git::Git.new
+    end
+        
+    context("index") do
+      
+      it "errors when an alias is not provided" do
+        with_git_initialized_project do |p|
+          write_file(File.join(p.pigscripts_path, "my_script.pig"))
+          stderr, stdout = execute("describe my_script", p)
+          stderr.should == <<-STDERR
+ !    Usage: mortar describe PIGSCRIPT ALIAS
+ !    Must specify PIGSCRIPT and ALIAS.
+STDERR
+        end
+      end
+
+      it "errors when no remote exists in the project" do
+        with_git_initialized_project do |p|
+          @git.git('remote rm mortar')
+          p.remote = nil
+          write_file(File.join(p.pigscripts_path, "my_script.pig"))
+          stderr, stdout = execute("describe my_script my_alias", p, @git)
+          stderr.should == <<-STDERR
+ !    Unable to find git remote for project myproject
+STDERR
+        end
+      end
+
+      it "errors when requested pigscript cannot be found" do
+        with_git_initialized_project do |p|
+          stderr, stdout = execute("describe does_not_exist my_alias", p, @git)
+          stderr.should == <<-STDERR
+ !    Unable to find pigscript does_not_exist
+ !    No pigscripts found
+ STDERR
+        end
+      end
+      
+      it "requests and reports on a successful describe" do
+        with_git_initialized_project do |p|
+          # stub api requests
+          describe_id = "c571a8c7f76a4fd4a67c103d753e2dd5"
+          describe_url = "https://api.mortardata.com/describe/#{describe_id}"
+          parameters = ["name"=>"key", "value"=>"value" ]
+          
+          mock(Mortar::Auth.api).post_describe("myproject", "my_script", "my_alias", is_a(String), :parameters => parameters) {Excon::Response.new(:body => {"describe_id" => describe_id})}
+          mock(Mortar::Auth.api).get_describe(describe_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status" => Mortar::API::Describe::STATUS_QUEUED})).ordered
+          mock(Mortar::Auth.api).get_describe(describe_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status" => Mortar::API::Describe::STATUS_GATEWAY_STARTING})).ordered
+          mock(Mortar::Auth.api).get_describe(describe_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status" => Mortar::API::Describe::STATUS_PROGRESS})).ordered
+          mock(Mortar::Auth.api).get_describe(describe_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status" => Mortar::API::Describe::STATUS_SUCCESS, "web_result_url" => describe_url})).ordered
+          
+          # stub launchy
+          mock(Launchy).open(describe_url) {Thread.new {}}
+                    
+          write_file(File.join(p.pigscripts_path, "my_script.pig"))
+          stderr, stdout = execute("describe my_script my_alias --polling_interval 0.05 -p key=value", p, @git)
+          stdout.should == <<-STDOUT
+Taking code snapshot... done
+Sending code snapshot to Mortar... done
+Starting describe... started
+
+\r\e[0KDescribe status: QUEUED /\r\e[0KDescribe status: GATEWAY_STARTING -\r\e[0KDescribe status: PROGRESS \\\r\e[0KDescribe status: SUCCESS  
+
+Results available at https://api.mortardata.com/describe/c571a8c7f76a4fd4a67c103d753e2dd5
+Opening web browser to show results... done
+STDOUT
+        end
+      end
+      
+      it "requests and reports on a failed describe" do
+        with_git_initialized_project do |p|
+          # stub api requests
+          describe_id = "c571a8c7f76a4fd4a67c103d753e2dd5"
+          
+          error_message = "This is my error message\nWith multiple lines."
+          line_number = 23
+          column_number = 32
+          error_type = 'PigError'
+          
+          mock(Mortar::Auth.api).post_describe("myproject", "my_script", "my_alias", is_a(String), :parameters => []) {Excon::Response.new(:body => {"describe_id" => describe_id})}
+          mock(Mortar::Auth.api).get_describe(describe_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status" => Mortar::API::Describe::STATUS_QUEUED})).ordered
+          mock(Mortar::Auth.api).get_describe(describe_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status" => Mortar::API::Describe::STATUS_FAILURE, 
+            "error_message" => error_message,
+            "line_number" => line_number,
+            "column_number" => column_number,
+            "error_type" => error_type})).ordered
+
+          write_file(File.join(p.pigscripts_path, "my_script.pig"))
+          stderr, stdout = execute("describe my_script my_alias --polling_interval 0.05", p, @git)
+          stdout.should == <<-STDOUT
+Taking code snapshot... done
+Sending code snapshot to Mortar... done
+Starting describe... started
+
+\r\e[0KDescribe status: QUEUED /\r\e[0KDescribe status: FAILURE  
+
+STDOUT
+          stderr.should == <<-STDERR
+ !    Describe failed with PigError at Line 23, Column 32:
+ !    
+ !    This is my error message
+ !    With multiple lines.
+STDERR
+        end
+      end
+    end
+  end
+end
