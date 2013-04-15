2f420f83972ae7d1f1a36a80f93f4b19dc8b0f2b
Jonathan Packer <jpacker@mortardata.com>
2013-04-15 18:28:44 -0400

    added tests for aliasless illustrate

--- spec/mortar/command/illustrate_spec.rb
+++ spec/mortar/command/illustrate_spec.rb
@@ -150,6 +150,40 @@ Results available at https://api.mortardata.com/illustrates/c571a8c7f76a4fd4a67c
 STDOUT
         end
       end
+
+      it "requests and reports on a successful illustrate without an alias" do
+        with_git_initialized_project do |p|
+          # stub api requests
+          illustrate_id = "c571a8c7f76a4fd4a67c103d753e2dd5"
+          illustrate_url = "https://api.mortardata.com/illustrates/#{illustrate_id}"
+          parameters = ["name"=>"key", "value"=>"value" ]
+          
+          # These don't test the validity of the error message, it only tests that the CLI can handle a message returned from the server
+          mock(Mortar::Auth.api).post_illustrate("myproject", "my_script", nil, false, is_a(String), :parameters => parameters) {Excon::Response.new(:body => {"illustrate_id" => illustrate_id})}
+          mock(Mortar::Auth.api).get_illustrate(illustrate_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status_code" => Mortar::API::Illustrate::STATUS_QUEUED,           "status_description" => "Pending"})).ordered
+          mock(Mortar::Auth.api).get_illustrate(illustrate_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status_code" => Mortar::API::Illustrate::STATUS_GATEWAY_STARTING, "status_description" => "GATEWAY_STARTING"})).ordered
+          mock(Mortar::Auth.api).get_illustrate(illustrate_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status_code" => Mortar::API::Illustrate::STATUS_PROGRESS,         "status_description" => "In progress"})).ordered
+          mock(Mortar::Auth.api).get_illustrate(illustrate_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status_code" => Mortar::API::Illustrate::STATUS_READING_DATA,     "status_description" => "Reading data"})).ordered
+          mock(Mortar::Auth.api).get_illustrate(illustrate_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status_code" => Mortar::API::Illustrate::STATUS_PRUNING_DATA,     "status_description" => "Pruning data"})).ordered
+          mock(Mortar::Auth.api).get_illustrate(illustrate_id, :exclude_result => true).returns(Excon::Response.new(:body => {"status_code" => Mortar::API::Illustrate::STATUS_SUCCESS,          "status_description" => "Succeeded", "web_result_url" => illustrate_url})).ordered
+          
+          # stub launchy
+          mock(Launchy).open(illustrate_url) {Thread.new {}}
+                    
+          write_file(File.join(p.pigscripts_path, "my_script.pig"))
+          stderr, stdout = execute("illustrate my_script --polling_interval 0.05 -p key=value", p, @git)
+          stdout.should == <<-STDOUT
+Taking code snapshot... done
+Sending code snapshot to Mortar... done
+Starting illustrate... done
+
+\r\e[0KStatus: Pending... /\r\e[0KStatus: GATEWAY_STARTING... -\r\e[0KStatus: In progress... \\\r\e[0KStatus: Reading data... |\r\e[0KStatus: Pruning data... /\r\e[0KStatus: Succeeded  
+
+Results available at https://api.mortardata.com/illustrates/c571a8c7f76a4fd4a67c103d753e2dd5
+Opening web browser to show results... done
+STDOUT
+        end
+      end
       
       it "requests and reports on a failed illustrate" do
         with_git_initialized_project do |p|
