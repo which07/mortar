cc4d9ac83312046f8f796c2931b5863eb2c8a08c
Jonathan Packer <jpacker@mortardata.com>
2013-05-21 14:58:36 -0400

    gitless cli per-user sync

--- lib/mortar/command/base.rb
+++ lib/mortar/command/base.rb
@@ -164,7 +164,7 @@ class Mortar::Command::Base
     File.open(".mortar-project-remote", "w") do |f|
       f.puts api_registration_result["git_url"]
     end
-    git.sync_gitless_project(project)
+    git.sync_gitless_project(project, "master")
   end
   
 protected
@@ -400,7 +400,7 @@ protected
   def sync_code_with_cloud
     # returns git_ref
     if project.gitless_project?
-      return git.sync_gitless_project(project)
+      return git.sync_gitless_project(project, Mortar::Auth.user + "-base")
     else
       validate_git_based_project!
       return git.create_and_push_snapshot_branch(project)
