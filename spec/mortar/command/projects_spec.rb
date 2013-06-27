8ffe9547063ea456ed9875442d5bfb9f8c2c18d3
Jonathan Packer <jpacker@mortardata.com>
2013-06-27 13:46:50 -0400

    fix unit test

--- spec/mortar/command/projects_spec.rb
+++ spec/mortar/command/projects_spec.rb
@@ -128,7 +128,6 @@ STDOUT
 \e[1;32m      create\e[0m  pigscripts
 \e[1;32m      create\e[0m  pigscripts/some_new_project.pig
 \e[1;32m      create\e[0m  controlscripts
-\e[1;32m      create\e[0m  controlscripts/.gitkeep
 \e[1;32m      create\e[0m  controlscripts/lib
 \e[1;32m      create\e[0m  controlscripts/lib/__init__.py
 \e[1;32m      create\e[0m  macros
@@ -144,7 +143,6 @@ STDOUT
 \e[1;32m      create\e[0m  udfs/java/.gitkeep
 \e[1;32m      create\e[0m  vendor
 \e[1;32m      create\e[0m  vendor/controlscripts
-\e[1;32m      create\e[0m  vendor/controlscripts/.gitkeep
 \e[1;32m      create\e[0m  vendor/controlscripts/lib
 \e[1;32m      create\e[0m  vendor/controlscripts/lib/__init__.py
 \e[1;32m      create\e[0m  vendor/pigscripts
